388dffd5062c74522ed86db7eacdef7b3c6d6789
Fred-Jan van der Eijken <fred-jan@myonlinestore.com>
2023-12-29 15:27:10 +0100

    Added day 13 part 2

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -24,74 +24,81 @@ impl Pattern {
         }
     }
 
-    pub fn reflection_rows(&self) -> usize {
-        self.reflection_index(&self.rows)
-            .and_then(|index| Some(index + 1))
-            .unwrap_or(0)
+    pub fn reflection_rows(&self) -> Vec<usize> {
+        self.reflection_indices(&self.rows)
+            .iter()
+            .map(|&index| index + 1)
+            .collect()
     }
 
-    pub fn reflection_cols(&self) -> usize {
-        self.reflection_index(&self.cols)
-            .and_then(|index| Some(index + 1))
-            .unwrap_or(0)
+    pub fn reflection_cols(&self) -> Vec<usize> {
+        self.reflection_indices(&self.cols)
+            .iter()
+            .map(|&index| index + 1)
+            .collect()
     }
 
-    pub fn fix_smudge(&self) -> Self {
+    // Returns (fixed summary, fixed pattern)
+    pub fn fix_smudge(&self) -> (usize, Self) {
         self.contents
             .chars()
             .enumerate()
-            .filter(|(_, char)| ".#".contains(*char)) // no newlines
-            .find_map(|(i, char)| {
-                let fixed_pattern = Self::load(
-                    self.contents
-                        .chars()
-                        .enumerate()
-                        .map(|(i2, char2)| {
-                            return if i2 == i {
-                                match char2 {
-                                    '.' => '#',
-                                    _ => '.',
-                                }
-                            } else {
-                                char2
-                            };
-                        })
-                        .collect(),
-                );
+            .find_map(|(i, char)| match char {
+                '.' | '#' => {
+                    let mut fixed_contents = self.contents.clone();
+                    fixed_contents.replace_range(i..i + 1, if char == '.' { "#" } else { "." });
+                    let fixed_pattern = Self::load(fixed_contents);
 
-                if fixed_pattern.summary() > 0
-                    && (fixed_pattern.reflection_rows() != self.reflection_rows()
-                        || fixed_pattern.reflection_cols() != self.reflection_cols())
-                {
-                    return Some(fixed_pattern);
+                    let old_reflection_rows = self.reflection_rows();
+                    let new_reflection_rows = fixed_pattern.reflection_rows();
+
+                    if !new_reflection_rows.is_empty() && new_reflection_rows != old_reflection_rows
+                    {
+                        return Some((
+                            new_reflection_rows
+                                .iter()
+                                .find(|rows| !old_reflection_rows.contains(rows))
+                                .unwrap()
+                                * 100,
+                            fixed_pattern,
+                        ));
+                    }
+
+                    let old_reflection_cols = self.reflection_cols();
+                    let new_reflection_cols = fixed_pattern.reflection_cols();
+
+                    if !new_reflection_cols.is_empty() && new_reflection_cols != old_reflection_cols
+                    {
+                        return Some((
+                            *new_reflection_cols
+                                .iter()
+                                .find(|cols| !old_reflection_cols.contains(cols))
+                                .unwrap(),
+                            fixed_pattern,
+                        ));
+                    }
+
+                    None
                 }
-
-                return None;
+                _ => None,
             })
             .unwrap()
-        // .unwrap_or(Self {
-        //     contents: self.contents.clone(),
-        //     rows: self.rows.clone(),
-        //     cols: self.cols.clone(),
-        // })
     }
 
     pub fn summary(&self) -> usize {
-        self.reflection_index(&self.rows)
-            .and_then(|index| Some((index + 1) * 100))
-            .or(self
-                .reflection_index(&self.cols)
-                .and_then(|index| Some(index + 1)))
-            .unwrap_or(0)
-
-        // self.reflection_cols() + self.reflection_rows() * 100
+        self.reflection_rows().first().unwrap_or(&0usize) * 100
+            + self.reflection_cols().first().unwrap_or(&0usize)
     }
 
-    fn reflection_index(&self, lines: &Vec<String>) -> Option<usize> {
-        lines[..lines.len() - 1]
+    fn reflection_indices(&self, lines: &Vec<String>) -> Vec<usize> {
+        lines[..lines.len() - 1] // All except the last (which has no following lines to compare with)
             .iter()
             .enumerate()
-            .find_map(|(index, _)| {
+            .filter_map(|(index, _)| {
+                // Basically cuts the lines at current index and flips the order of preceding lines. Then zips this with
+                // the following lines and iterates through the zipped lists element-wise to check if they are equal:
+                //
+                // ABCDEFGH --cut-in-half-> ABCD,EFGH --flip left--> DCBA,EFGH --compare-index-wise-> D vs E, C vs F, ..
                 let reflected_count = lines[0..index + 1]
                     .iter()
                     .rev()
@@ -101,13 +108,14 @@ impl Pattern {
                 let lines_left = index + 1;
                 let lines_right = lines.len() - (index + 1);
 
-                // Left lines exhausted -> perfect reflection
+                // Preceding (left) or following (right) lines exhausted -> perfect reflection
                 if reflected_count == lines_left || reflected_count == lines_right {
                     return Some(index);
                 }
 
                 None
             })
+            .collect()
     }
 }
 
@@ -135,7 +143,7 @@ impl Puzzle {
     pub fn part2(&self) -> usize {
         self.patterns
             .iter()
-            .fold(0, |acc, pattern| acc + pattern.fix_smudge().summary())
+            .fold(0, |acc, pattern| acc + pattern.fix_smudge().0)
     }
 }
 
@@ -145,7 +153,7 @@ fn main() {
         "Part 1: {}",
         Puzzle::load(fs::read_to_string("inputs/day13.txt").unwrap()).part1()
     );
-    // Wrong: 38923 (too high)
+    // 25401
     println!(
         "Part 2: {}",
         Puzzle::load(fs::read_to_string("inputs/day13.txt").unwrap()).part2()
@@ -160,19 +168,21 @@ mod tests {
     fn test_reflection() {
         let puzzle = &Puzzle::load(fs::read_to_string("samples/day13a.txt").unwrap());
 
-        assert_eq!(puzzle.patterns[0].reflection_cols(), 5);
-        assert_eq!(puzzle.patterns[1].reflection_rows(), 4);
+        assert_eq!(puzzle.patterns[0].reflection_cols(), vec![5]);
+        assert_eq!(puzzle.patterns[1].reflection_rows(), vec![4]);
 
         let puzzle = &Puzzle::load(fs::read_to_string("samples/day13b.txt").unwrap());
 
-        assert_eq!(puzzle.patterns[0].reflection_rows(), 0);
-        assert_eq!(puzzle.patterns[0].reflection_cols(), 3);
+        assert_eq!(puzzle.patterns[0].reflection_rows(), vec![]);
+        assert_eq!(puzzle.patterns[0].reflection_cols(), vec![3]);
 
-        assert_eq!(puzzle.patterns[1].reflection_rows(), 0);
-        assert_eq!(puzzle.patterns[1].reflection_cols(), 11);
+        assert_eq!(puzzle.patterns[1].reflection_rows(), vec![]);
+        assert_eq!(puzzle.patterns[1].reflection_cols(), vec![11]);
 
-        assert_eq!(puzzle.patterns[2].reflection_rows(), 0);
-        assert_eq!(puzzle.patterns[2].reflection_cols(), 1);
+        assert_eq!(puzzle.patterns[2].reflection_rows(), vec![]);
+        assert_eq!(puzzle.patterns[2].reflection_cols(), vec![1]);
+
+        assert_eq!(puzzle.patterns[3].reflection_cols(), vec![1, 10]); // has multiple reflections
     }
 
     #[test]
@@ -187,8 +197,13 @@ mod tests {
     fn test_part2() {
         let puzzle = &Puzzle::load(fs::read_to_string("samples/day13a.txt").unwrap());
 
-        assert_eq!(puzzle.patterns[0].fix_smudge().reflection_rows(), 3);
-        assert_eq!(puzzle.patterns[1].fix_smudge().reflection_rows(), 1);
+        assert_eq!(puzzle.patterns[0].fix_smudge().1.reflection_rows(), vec![3]);
+        assert_eq!(puzzle.patterns[0].fix_smudge().1.reflection_cols(), vec![5]); // still a valid reflection after fix
+
+        assert_eq!(
+            puzzle.patterns[1].fix_smudge().1.reflection_rows(),
+            vec![1, 4]
+        );
 
         assert_eq!(puzzle.part2(), 400);
     }
